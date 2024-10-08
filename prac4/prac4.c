ec504b7d8734d1338516da992b510d226f6f08f7
FacelessProfile <135810230+FacelessProfile@users.noreply.github.com>
2024-10-08 13:26:20 +0700

    Update prac4.c

--- prac4/prac4.c
+++ prac4/prac4.c
@@ -4,10 +4,10 @@ void main() {
     printf("Input height: ");
     scanf("%d",&height);
     for (i=0;i<height;i++) {
-        for (k =0;k<i;k++) {
+        for (k=0;k<i;k++) {
             printf(" ");
         }
-        for (j =0;j<height-i;j++) {
+        for (j=0;j<height-i;j++) {
             printf("%c",65+i);
         }
         printf("\n");
