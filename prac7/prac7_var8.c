4c08c9012d82922b304a394562b73002df763c92
FacelessProfile <135810230+FacelessProfile@users.noreply.github.com>
2024-10-27 21:02:28 +0700

    Added prac 7
    IF RAIN IS WHAT YOU WANT JUST CLOSE YOUR EYES.   (LETTERS: C F H J L)
    AS I CLOSE MY EYES I FEEL IT ALL SLIPPING AWAY.       (LETTERS: C F G M N O T W)

---
+++ prac7/prac7_var8.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+void bubble_sort(int arr[], int n) {
+    for (int i=0;i<n-1;i++) {
+        for (int j=0;j<n-i-1;j++) {
+            if (arr[j]>arr[j+1]) {
+                int temp=arr[j];
+                arr[j]=arr[j + 1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+void find_unique_chars(char *str) {
+    int n=strlen(str);
+    int ascii_values[n];
+    int count[128]= {0};
+
+    int index=0;
+    for (int i=0;i<n;i++) {
+        if (str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z') {
+            int ascii_val=(int)str[i];
+            ascii_values[index++]=ascii_val;
+            count[ascii_val]++;
+        }
+    }
+
+    bubble_sort(ascii_values, index);
+
+    printf("Characters that appear only once:\n");
+    for (int i=0;i<index;i++) {
+        if (count[ascii_values[i]]==1 && (i==0 || ascii_values[i]!=ascii_values[i-1])) {
+            printf("%c ", (char)ascii_values[i]);
+        }
+    }
+    printf("\n");
+}
+
+void main() {
+    char input[256];
+    printf("Enter sentence: ");
+    fgets(input, 256, stdin);
+    find_unique_chars(input);
+}
