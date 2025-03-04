bea2823b6090dfb133a1ec0f1b9bb6c7947861fc
FacelessProfile <nikitos1992xxx@gmail.com>
2025-03-04 08:48:24 +0700

    added a sort for git lab task

---
+++ sort.c
@@ -0,0 +1,15 @@
+void selectionSort(struct Student arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int maxIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j].total > arr[maxIndex].total) {
+                maxIndex = j;
+            }
+        }
+        if (maxIndex != i) {
+            struct Student temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+        }
+    }
+}
